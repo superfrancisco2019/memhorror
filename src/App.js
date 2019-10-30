4c690db068bb441eae4ec93a82a9b5b4b1451537
FPaurisse <paurissefrederic@gmail.com>
2019-10-30 12:58:01 +0100

    recept components in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import PlayButton from "./components/PlayButton/PlayButton";
+import GetCards from "./components/GetCards/GetCards";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      play: false
+    }
+  }
+  
+  render() {
+    const { play } = this.state
+    return (
+      <div className="App">
+        <PlayButton playOnClick={(play) => this.setState({ play })} />
+        {play && <GetCards />}
+      </div>
+    );
+  }
 }
 
 export default App;
