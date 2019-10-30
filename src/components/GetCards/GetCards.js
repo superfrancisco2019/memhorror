e8de3bb42e401be8b4e0288165cb09f2cede764d
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2019-10-30 13:52:42 +0100

    add api data

---
+++ src/components/GetCards/GetCards.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+
+class GetCards extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: ""
+        };
+        this.getCards = this.getCards.bind(this);
+    }
+
+    componentDidMount() {
+        this.getCards()
+    }
+
+    getCards() {
+        // Send the request
+        axios.get('https://hackathon-wild-hackoween.herokuapp.com/movies')
+          // Extract the DATA from the received response
+          .then(response => response.data)
+          // Use this data to update the state
+          .then(data => {
+            this.setState({
+              cards: data
+            });
+        });
+    }
+    
+    render() {
+        return (
+            <div className="GetCards">
+            </div>
+        )
+    }
+}
+
+
+export default GetCards;
