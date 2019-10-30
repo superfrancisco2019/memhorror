2ca7fa8db409788feb144e7ebf77107765c182cf
Antoine Ribeiro <antoine.ribeiro37@gmail.com>
2019-10-30 18:04:30 +0100

    clean code and add randomcards

--- src/components/GetCards/GetCards.js
+++ src/components/GetCards/GetCards.js
@@ -5,23 +5,13 @@ import DisplayCards from "../DisplayCards/DisplayCards";
 class GetCards extends Component {
   constructor(props) {
     super(props);
-<<<<<<< HEAD
-      this.state = {
-        play: false,
-        cards: null
-=======
     this.state = {
-      play: false,
       cards: null
->>>>>>> 11af655bfc8ee4053dc73abecc580422fb2b17fd
     };
     this.getCards = this.getCards.bind(this);
   }
 
   componentDidMount() {
-    this.setState({
-      play: this.props.play
-    });
     this.getCards();
   }
 
@@ -40,7 +30,7 @@ class GetCards extends Component {
     const { cards } = this.state;
     return (
       <div className="GetCards">
-            {cards && <DisplayCards dataCards={cards} />}
+            {cards && <DisplayCards cards={cards} />}
       </div>
     );
   }
