c886ef16d745cbfe3ec93afb345f5e66fa98a9e6
Nombalier <a.nombalier@gmail.com>
2019-10-30 14:33:54 +0100

    add DisplayCards class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import DisplayCards from './components/DisplayCards/DisplayCards';
 
 function App() {
   return (
     <div className="App">
-      
+      <DisplayCards />
     </div>
   );
 }
---
+++ src/components/DisplayCards/DisplayCards.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './DisplayCards.css';
+
+class DisplayCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="card">
+
+      </div>
+
+    );
+  }
+}
+
+export default DisplayCards;
