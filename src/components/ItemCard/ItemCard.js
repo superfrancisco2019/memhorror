5fa8d63fae69e57535fdb6b77bcffbca8412a7dc
François Boisselier <francois.boisselier@gmail.com>
2019-10-31 12:26:49 +0100

    add effect cart

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -5,16 +5,20 @@ class ItemCard extends Component {
   constructor(props) {
       super(props);
       this.clickCard = this.clickCard.bind(this)
+      this.state = { hidden : true}
   }
   
   clickCard() {
       this.props.compareId(this.props.idCard)
+      this.setState({ hidden: false })
   }
     
   render() {
     return (
         <div className="ItemCard" key={this.props.idCard}>
-          <img onClick={this.clickCard} src={this.props.imageCard} alt="/" />
+          <div className={this.state.hidden ? "image-container hidden" : "image-container visible"}>
+            <img onClick={this.clickCard} src={this.props.imageCard} alt="/"/>     
+          </div>
         </div>
     );
   }
